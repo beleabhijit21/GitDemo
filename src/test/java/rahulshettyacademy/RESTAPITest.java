1e322e742d96f444d41ed6fefa100b13fe566875
beleabhijit21 <beleabhijit21@gmail.com>
2022-04-07 23:33:44 +0530

    first commit

---
+++ src/test/java/rahulshettyacademy/RESTAPITest.java
@@ -0,0 +1,20 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira");
+	}
+	
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/rahulshettyacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from maven
+	//how to execute all test cases from test folder - mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+
+}
